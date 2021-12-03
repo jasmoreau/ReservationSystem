4f0d0abb3b682a9205366a73876ba91d062308c7
TuanTran <tmtran38@uh.edu>
2021-12-03 15:57:46 -0600

    Update

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Text } from '../ReservationBox/ReservationBoxElements';
 import {BasicTable}  from './BasicTable';
 
 export const Display = (props) => {
@@ -6,7 +7,7 @@ export const Display = (props) => {
     return(
     <>
     <div>
-        {props.highTraffic && <p>WARNING: This is a high traffic day / holiday ({props.highTraffic}) which has a $10 no show fee!</p>}
+        {props.highTraffic && <><Text>WARNING: This is a high traffic day / holiday ({props.highTraffic}) which has a $10 no show fee!</Text><br/></>}
         {props.cannotSeat && <p cannotSeat>We apologize, but we cannot seat you for this time!</p>}
         {props.showMessage && !props.cannotSeat && (<BasicTable data={props.data} dateT={props.dateT} userData={props.userData} highTraffic={props.highTraffic} reservationPlaced={props.reservationPlaced} setReservationPlaced={props.setReservationPlaced}></BasicTable>)}
     </div>
